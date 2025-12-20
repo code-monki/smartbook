9c1f2a3d2886d9ceb2aef99451763da42951b7dd
Chuck McKnight <cmcknight@users.noreply.github.com>
2025-12-20 14:24:50 -0800

    fix(reader): Mark unused parameters in WebChannelBridge
    All parameters are reserved for future implementation.
    Fixes -Werror unused-parameter warnings.

--- reader/src/WebChannelBridge.cpp
+++ reader/src/WebChannelBridge.cpp
@@ -16,7 +16,7 @@ void WebChannelBridge::setupWebChannel(QWebChannel* webChannel) {
     }
 }
 
-void WebChannelBridge::saveFormData(const QString& formId, const QString& dataJson, const QString& callback) {
+void WebChannelBridge::saveFormData(const QString& formId, const QString& /* dataJson */, const QString& /* callback */) {
     qDebug() << "saveFormData called:" << formId;
     // TODO: Implement form data saving
     emit formDataSaved(formId, true, QString());
@@ -28,31 +28,31 @@ void WebChannelBridge::loadFormData(const QString& formId, const QString& callba
     emit formDataLoaded(formId, QString(), QString());
 }
 
-void WebChannelBridge::requestAppConsent(const QString& appId, const QString& callback) {
+void WebChannelBridge::requestAppConsent(const QString& appId, const QString& /* callback */) {
     qDebug() << "requestAppConsent called:" << appId;
     // TODO: Implement consent dialog
     emit consentGranted(appId, false);
 }
 
-void WebChannelBridge::saveSandboxFile(const QString& filename, const QByteArray& data, const QString& callback) {
+void WebChannelBridge::saveSandboxFile(const QString& filename, const QByteArray& /* data */, const QString& /* callback */) {
     qDebug() << "saveSandboxFile called:" << filename;
     // TODO: Implement sandbox file saving
     emit sandboxFileSaved(filename, true, QString());
 }
 
-void WebChannelBridge::loadSandboxFile(const QString& filename, const QString& callback) {
+void WebChannelBridge::loadSandboxFile(const QString& filename, const QString& /* callback */) {
     qDebug() << "loadSandboxFile called:" << filename;
     // TODO: Implement sandbox file loading
     emit sandboxFileLoaded(filename, QByteArray(), QString());
 }
 
-void WebChannelBridge::listSandboxFiles(const QString& callback) {
+void WebChannelBridge::listSandboxFiles(const QString& /* callback */) {
     qDebug() << "listSandboxFiles called";
     // TODO: Implement sandbox file listing
     emit sandboxFilesListed(QStringList(), QString());
 }
 
-void WebChannelBridge::deleteSandboxFile(const QString& filename, const QString& callback) {
+void WebChannelBridge::deleteSandboxFile(const QString& filename, const QString& /* callback */) {
     qDebug() << "deleteSandboxFile called:" << filename;
     // TODO: Implement sandbox file deletion
     emit sandboxFileDeleted(filename, true, QString());
